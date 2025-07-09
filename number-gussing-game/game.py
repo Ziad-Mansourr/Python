1703dc3bf1354446a1b38dcdb90c6f797ea85000
Ziad-Mansourr <01556746404ziad@gmail.com>
2025-07-09 18:20:53 +0300

    update

--- number-gussing-game/game.py
+++ number-gussing-game/game.py
@@ -1,13 +1,23 @@
 import random
-guss_number = random.randint(1, 100)
 while 1:
-    try:
-        number = int(input('Guess the number between 1 and 100: '))
-        if (number > guss_number):
-            print("Too high")
-        elif number < guss_number:
-            print("Too low")
-        else:
-            print("Congratulations! You guessed the number.")
-    except ValueError:
-        print("Please enter a valid number")
+    count = 1
+    guss_number = random.randint(1, 100)
+    while 1:
+        try:
+            number = int(input('Guess the number between 1 and 100: '))
+            if count == 5:
+                print(f"Number is {guss_number}")
+                print("You lose! Thanks")
+                print("You can guess again")
+                break
+            if (number > guss_number):
+                count+=1
+                print("Too high")
+            elif number < guss_number:
+                count+=1
+                print("Too low")
+            else:
+                print("Congratulations! You guessed the number.")
+                break
+        except ValueError:
+            print("Please enter a valid number")
